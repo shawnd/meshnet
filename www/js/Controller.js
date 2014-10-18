24f28356b177bdbf941a2fd607777d741a015a8f
Shawn Dhawan <gsdhawan@sfu.ca>
2014-10-17 21:20:12 -0700

    Setup BLE plugin

---
+++ www/js/Controller.js
@@ -0,0 +1,12 @@
+// Controller
+$(document).ready(function(){
+	console.log("ready");
+
+	// ble.scan([], 15, function(){
+	// 	console.log("SUCCESS");
+	// 	menu.toggle();
+	// }, function(){
+	// 	console.log("FAIURE");
+	// 	menu.toggle();
+	// });
+});
