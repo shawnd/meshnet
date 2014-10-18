1d2439ad8e8e564921e2f55d2f7d49c894894406
Shawn Dhawan <gsdhawan@sfu.ca>
2014-10-18 00:03:47 -0700

    Device listing on discovery

--- www/js/index.js
+++ www/js/index.js
@@ -1,26 +1,37 @@
 // Controller
-var deviceList;
+var deviceList = [];
+
 var app = {
     initialize: function() {
-    	deviceList = $("#deviceList");
     	this.bindEvents();
-    	alert("STARTED");
     },
     bindEvents: function() {
 		document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
         app.refreshDeviceList();
-        alert("ready");
     },
     refreshDeviceList: function() {
     	// scan for all devices
-        ble.scan([], 5, app.onDiscoverDevice, app.onError);
+    	setInterval(function(){
+    		ble.scan([], 5, app.onDiscoverDevice, app.onError);
+    	}, 10000); //90 second scan
     },
     onDiscoverDevice: function(device) {
-        alert("FOUND: " + device.id);
+    	var stored = false;
+    	for(current in deviceList){
+    		if(deviceList[current].id == device.id){
+    			stored = true;
+    		}
+    	}
+    	if(stored == false){
+	    	deviceList[device.id] = device;
+	    	console.log(deviceList[device.id]);
+	        alert("FOUND: " + device.name + " " + deviceList[device.id].id);
+	    }
     },
     onError: function(error){
-    	alert("ERROR: " + error);
+    	console.log("ERROR: " + error);
+    	//possibly send error to server via api for debugging/error logging purposes
     }
 };
