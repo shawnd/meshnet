8719a4b1a76cf5435e9359963fe618db439e920b
Shawn Dhawan <gsdhawan@sfu.ca>
2014-10-17 21:52:57 -0700

    Removed platforms

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -1,12 +1,14 @@
 // Controller
 $(document).ready(function(){
 	console.log("ready");
+	menu.toggle();
 
-	// ble.scan([], 15, function(){
-	// 	console.log("SUCCESS");
-	// 	menu.toggle();
-	// }, function(){
-	// 	console.log("FAIURE");
-	// 	menu.toggle();
-	// });
+	ble.scan([], 5, function(device) {
+	    console.log(JSON.stringify(device));
+	    setTimeout(function(){
+	    	menu.toggle();
+	    }, 5000);
+	}, function(device){
+		menu.toggle();
+	});
 });
